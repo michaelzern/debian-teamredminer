1591684f30f60b41335dc89e71909b067db732e7
mikez <11712290+michaelzern@users.noreply.github.com>
2023-04-11 09:36:20 -0500

    Update README.md: Improve header consistency, formatting, and add GitHub link

--- README.md
+++ README.md
@@ -1,14 +1,47 @@
 # Auto setup for TeamRedMiner and OpenCL ROCr drivers tested on Ubuntu 20.04
 
-Setup is using nicehash, can add in preferred pool
-Edit inside settings/amdeth.sh
+This script automates the setup process for TeamRedMiner and OpenCL ROCr drivers on Ubuntu 20.04. The miner is configured to work with NiceHash by default, but you can easily change the pool settings.
+
+## Prerequisites
+
+- Ubuntu 20.04
+- AMD GPU(s) compatible with TeamRedMiner
+
+## Download TeamRedMiner
+
+Download the latest version of TeamRedMiner from the official GitHub repository's releases page:
+
+[https://github.com/todxx/teamredminer/releases](https://github.com/todxx/teamredminer/releases)
+
+Place the downloaded `teamredminer-*.tgz` file in the same directory as the setup script.
+
+## Configure the miner
+
+Edit the `settings/amdeth.sh` file to configure the miner with your preferred pool and worker settings.
+
+## Running the script
+
+Execute the setup script:
+
+```bash
+./nhAMD.sh
+```
 
 # Verify miner working
-
+To verify the miner is working, use the following commands:
 ```
-# list screen
+# List all active screen sessions
 screen -ls
-# return to running screen
+
+# Return to the running screen session (if only one session is active)
 screen -r
+
+# If multiple sessions are active, use the session ID from the `screen -ls` output
+screen -r session_id
 ```
-# Detach from screen by pressing ctrl+a then ctrl+d
+
+Detach from the screen session by pressing Ctrl+A followed by Ctrl+D
+
+# Troubleshooting
+
+If you encounter any issues, ensure that your system meets the prerequisites and that you have downloaded the correct version of TeamRedMiner for your GPU(s). Consult the [TeamRedMiner GitHub repository](https://github.com/todxx/teamredminer) for additional documentation and support.
--- nhAMD.sh
+++ nhAMD.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 DIR=/etc/OpenCL/vendors
 FILES=$SCRIPT_DIR/trm
 SETTINGS=$SCRIPT_DIR/settings
 
 package="screen"
 
-#install dependencies
+# Install dependencies
 for packageName in $package; do
   dpkg -l | grep -qw $packageName || sudo apt install -y $packageName
 done
 
-#install drivers seperately
+# Install drivers separately
 if [ -d "$DIR" ]; then
     echo "Drivers already installed"
-    else
+else
     TEMP_DEB="$(mktemp)" &&
     wget -O "$TEMP_DEB" 'https://repo.radeon.com/amdgpu-install/latest/ubuntu/focal/' &&
-    sudo dpkg -i "$TEMP_DEB"
+    sudo dpkg -i "$TEMP_DEB" || { echo "Failed to install drivers"; exit 1; }
     rm -f "$TEMP_DEB"
 
-    sudo amdgpu-install -y --usecase=opencl --opencl=rocr --accept-eula
+    sudo amdgpu-install -y --usecase=opencl --opencl=rocr --accept-eula || { echo "Failed to install OpenCL"; exit 1; }
 fi
 
-# extract
+# Extract TeamRedMiner
 if [ -d "$FILES" ]; then
     echo "TeamRedMiner already installed"
-    else
-      sudo mkdir $SCRIPT_DIR/trm
-      sudo tar -xvzf teamredminer*.tgz -C $FILES
+else
+    sudo mkdir $SCRIPT_DIR/trm
+    sudo tar -xvzf teamredminer*.tgz -C $FILES || { echo "Failed to extract TeamRedMiner"; exit 1; }
 fi
 
-#copy config file
-sudo cp $SETTINGS/amdeth.sh $FILES/teamredminer*/
+# Copy config file
+sudo cp $SETTINGS/amdeth.sh $FILES/teamredminer*/ || { echo "Failed to copy config file"; exit 1; }
 sudo chmod +x $FILES/teamredminer*/amdeth.sh
 
-#start miner
-echo "starting miner, run screen -r to attach"
-screen -S miner -dm bash -c '$FILES/teamredminer*/amdeth.sh'
+# Start miner
+echo "Starting miner, run screen -r to attach"
+screen -S miner -dm bash -c '$FILES/teamredminer*/amdeth.sh' || { echo "Failed to start miner"; exit 1; }
